e05270acfe1dae426cc29884337c04ae85b84274
trudnitskaya <trudnitskaya@dpkapp.ru>
2022-04-07 22:37:22 +0300

    Добавьте файлы проекта.

---
+++ SinglyLinkedList-lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList_lib
+{
+    public class Class1
+    {
+    }
+}
